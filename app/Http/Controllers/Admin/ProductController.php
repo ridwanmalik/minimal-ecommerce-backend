2a3895f7686178ea1f2f4ee61950912d5947a8f6
Ridwan Malik <34468231+ridwanmalik@users.noreply.github.com>
2022-08-14 14:17:53 +0600

    Update ProductController.php

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -17,7 +17,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $products = Product::all();
+        $products = Product::latest()->get();
         return view('admin.pages.products.index', compact('products'));
     }
 
